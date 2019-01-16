a18e326101c9d596be51c9ed4172e7fe61eec99c
Arnold Patten <apatten001@yahoo.com>
2019-01-15 20:42:31 -0500

    start app for messages from users.

---
+++ contact/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+
+    """
+    Store contact information
+    """
+
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    email = models.EmailField()
+    subject = models.CharField(max_length=500)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.subject
