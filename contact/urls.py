b61634323193444b0089f9374e5914280b8c33a4
Arnold Patten <apatten001@yahoo.com>
2019-01-17 19:14:30 -0500

    add contact app for contacts

---
+++ contact/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import add_contact
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('contact/', add_contact, name='add-contact')
+
+
+]
+
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 # Create your views here.
+
+
+def add_contact(request):
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('houses:home')
+    else:
+        form = ContactForm()
+        return render(request, 'houses/contact.html', {'form': form})
