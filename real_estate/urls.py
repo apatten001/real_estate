888edd77e422067c087dfc56bbfdd684268e89bc
Arnold Patten <apatten001@yahoo.com>
2019-01-24 22:15:59 -0500

    update urls and redirect urls in settings

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
     path('api/', include(router.urls)),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('houses.urls')),
     path('', include('news.urls')),
