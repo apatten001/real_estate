bff8b1839f426f6fbff1706f65b4356d30bd2c43
Arnold Patten <apatten001@yahoo.com>
2019-01-24 22:19:05 -0500

    edit views for frequently asked questions

--- faqs/views.py
+++ faqs/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
-from .models import  FrequentQuestionsAnswers
-from .serializers import FAQSerializer
-
+from django.views.generic.edit import FormMixin
 from rest_framework import viewsets
+
+from contact.forms import SubscribeForm
+from .models import FrequentQuestionsAnswers
+from .serializers import FAQSerializer
 
 
 # Create your views here.
 
 
-class FAQListView(ListView):
+class FAQListView(FormMixin, ListView):
 
+    form_class = SubscribeForm
     model = FrequentQuestionsAnswers
     template_name = 'houses/faqs.html'
 
     paginate_by = 2
+
+
+def add_subscriber(request):
+
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            subscribe = form.save()
+            subscribe.save()
+            return redirect('houses:home')
+    else:
+        form = SubscribeForm()
+        return render(request, 'houses/faqs.html', {'form': form})
 
 
 class FAQViewSet(viewsets.ModelViewSet):
