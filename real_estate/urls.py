214761efc3b5701e3974a5ebecd76ae606aa5ce6
Arnold Patten <apatten001@yahoo.com>
2019-01-27 13:16:56 -0500

    add version number to api

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -33,7 +33,7 @@ router.register(r'contacts', ContactViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
-    path('api/', include(router.urls)),
+    path('api/v1/', include(router.urls)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('houses.urls')),
